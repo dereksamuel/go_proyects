487c7c66cd4e34ef3a6bf4927fdabbb965575a13
Derek Paul <11dereksamuel@gmail.com>
2023-02-03 12:09:36 -0500

    done channels

---
+++ goservers.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	started := time.Now()
+	channel := make(chan string)
+
+	servers := []string{
+		"http://platzi.com",
+		"http://google.com",
+		"http://facebook.com",
+		"http://instagram.com",
+	}
+
+	for _, server := range servers {
+		go checkServerGo(server, channel)
+	}
+
+	for i := 0; i < len(servers); i++ {
+		fmt.Println(<-channel)
+	} // va a esperar a que todos los goroutines terminen
+	finished := time.Since(started)
+
+	fmt.Printf("Current time is: %s\n", finished)
+}
+
+func checkServerGo(server string, channel chan string) {
+	_, err := http.Get(server)
+
+	if err != nil {
+		channel <- "[server-" + server + "-ERROR]"
+		return
+	}
+
+	channel <- "[server-" + server + "-OK]"
+}
